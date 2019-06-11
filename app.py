ec53946878292ac8bbf0cde09958afa60ea938dc
Schneider, S.A. (Simon) <s.a.schneider@uu.nl>
2019-06-11 19:37:48 +0200

    labels aktualisiert

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ from bokeh.layouts import row, column
 from bokeh.models import ColumnDataSource
 # from bokeh.models.widgets import Slider, TextInput
 from bokeh.models import Button
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show
+from bokeh.models.annotations import Title
+
+from functools import partial
+
 import json
 import urllib2
 import numpy as np
@@ -23,12 +27,6 @@ response = urllib2.urlopen(url)
 # Set up data
 html = response.read()
 data_feed = json.loads(html)
-
-# with open('data.json', 'r') as fh:
-#     content = fh.readlines()
-#
-# data_feed = json.loads(content[0])
-
 data = {'field1': [],
         'field2': [],
         'field3': [],
@@ -37,6 +35,14 @@ data = {'field1': [],
         'field6': [],
         'field7': [],
         'field8': []}
+labels = {'field1': 'PM10',
+          'field2': 'PM2.5',
+          'field3': 'Temperatur Aussen',
+          'field4': 'Luftfeuchtigkeit Aussen',
+          'field5': 'Niederschlag',
+          'field6': 'Luftdruck',
+          'field7': 'Temperatur Innen',
+          'field8': 'Luftfeuchtigkeit Innen'}
 
 for entry in data_feed['feeds']:
     for key, values in entry.iteritems():
@@ -47,13 +53,14 @@ for entry in data_feed['feeds']:
                 data[key] = np.nan
 
 # N = 200
-x = range(len(data['field7']))
-y = data['field7']
+init_field = 'field7'
+x = range(len(data[init_field]))
+y = data[init_field]
 source = ColumnDataSource(data=dict(x=x, y=y))
 
 
 # Set up plot
-plot = figure(plot_height=800, plot_width=800, title="Wetterstation",
+plot = figure(plot_height=800, plot_width=800, title=labels[init_field],
               tools="crosshair,pan,reset,save,wheel_zoom")
 
 plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
@@ -62,7 +69,7 @@ plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 i = 0
 
 
-def update_data():
+def update_data(inkey):
     url = 'https://api.thingspeak.com/channels/794255/'
     url += 'feeds.json?api_key=0AWEFXGCB5L3T4LC'
     response = urllib2.urlopen(url)
@@ -73,32 +80,40 @@ def update_data():
     d = []
     for entry in data_feed['feeds']:
         for key, values in entry.iteritems():
-            if key == 'field7':
+            if key == inkey:
                 try:
                     d.append(float(values))
                 except Exception:
-                    d = np.nan
+                    d = None
 
-    x = range(len(d))
-    y = d
-    source.data = dict(x=x, y=y)
+    if d is not None:
+        x = range(len(d))
+        y = d
+        plot.title.text = labels[inkey]
+        source.data = dict(x=x, y=y)
 
 
 # # Set up layouts and add to document
-button1 = Button(label="PM10: %s" % data['field1'])
-button2 = Button(label="PM2.5: %s" % data['field2'])
-button3 = Button(label="Temperatur Aussen")
-button4 = Button(label="Luftfeuchtigkeit Aussen")
-button5 = Button(label="Niederschlag")
-button6 = Button(label="Luftdruck")
-button7 = Button(label="Temperatur Innen: %.2f" % data['field7'][-1])
-button8 = Button(label="Luftfeuchtigkeit Innen: %.2f" % data['field8'][-1])
+button1 = Button(label="%s: %s" % (labels['field1'], data['field1']))
+button2 = Button(label="%s: %s" % (labels['field2'], data['field2']))
+button3 = Button(label="%s" % labels['field3'])
+button4 = Button(label="%s" % labels['field4'])
+button5 = Button(label="%s" % labels['field5'])
+button6 = Button(label="%s" % labels['field6'])
+button7 = Button(label="%s: %.2f" % (labels['field7'], data['field7'][-1]))
+button8 = Button(label="%s: %.2f" % (labels['field8'], data['field8'][-1]))
 
-button7.on_click(update_data)
-button8.on_click(update_data)
+button1.on_click(partial(update_data, inkey="field1"))
+button2.on_click(partial(update_data, inkey="field2"))
+button3.on_click(partial(update_data, inkey="field3"))
+button4.on_click(partial(update_data, inkey="field4"))
+button5.on_click(partial(update_data, inkey="field5"))
+button6.on_click(partial(update_data, inkey="field6"))
+button7.on_click(partial(update_data, inkey="field7"))
+button8.on_click(partial(update_data, inkey="field8"))
 
 inputs = column(button1, button2, button3, button4, button5, button6,
                 button7, button8)  # , offset, amplitude, phase, freq)
 
 curdoc().add_root(row(inputs, plot, width=800))
-curdoc().title = "Sliders"
+curdoc().title = "Wetterstation"
